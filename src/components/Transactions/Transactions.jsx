28d4a377bd02994fa2bc9403394f234d3d6ad1c5
Nikolai-desig <rinzleramad79@gmail.com>
2023-04-30 20:36:22 +0300

    Update Transactions.jsx

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -1,4 +1,4 @@
-import PropTypes, { func } from 'prop-types';
+import PropTypes from 'prop-types';
 import css from 'components/Transactions/Transactions.module.css'
 import transactions from 'data/transactions.json';
 
